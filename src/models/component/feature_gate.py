61816dd8adf1a3020791c48e7de10b5e66cb9a93
EdwinWang37 <58389414+EdwinWang37@users.noreply.github.com>
2024-06-17 09:23:40 +0800

    Update feature_gate.py

--- src/models/component/feature_gate.py
+++ src/models/component/feature_gate.py
@@ -6,15 +6,15 @@ class FeatureGate(nn.Module):
     def __init__(self, cfg):
         super(FeatureGate, self).__init__()
         self.num_units = 400
-        # 由于输入已经是[batch_size, time, units]，直接对其进行处理
-        self.f_dense = nn.Linear(self.num_units * 2, self.num_units)  # 输入维度翻倍
+     
+        self.f_dense = nn.Linear(self.num_units * 2, self.num_units)  
         self.g_dense = nn.Linear(self.num_units * 2, self.num_units)
 
     def forward(self, short_rep, long_rep):
         # short_rep, long_rep: [batch_size, time, units] = [16, 50, 400]
 
         # Concatenation
-        f_input = torch.cat([short_rep, long_rep], dim=-1)  # 在最后一个维度上合并
+        f_input = torch.cat([short_rep, long_rep], dim=-1)  
         g_input = torch.cat([short_rep, long_rep], dim=-1)
 
         # Transformation
