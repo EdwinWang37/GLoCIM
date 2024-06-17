2fe76789fd3019a93b714fe9db0b1108d8a59978
EdwinWang37 <58389414+EdwinWang37@users.noreply.github.com>
2024-06-17 09:20:42 +0800

    Update pre_news_encoder.py

--- src/models/component/pre_news_encoder.py
+++ src/models/component/pre_news_encoder.py
@@ -30,7 +30,7 @@ class PreNewsEncoder(nn.Module):
 
         self.view_size = [cfg.model.title_size, cfg.model.abstract_size]
 
-        # 更新attention层的输入维度，考虑到category和subcategory的嵌入
+        
         attention_input_dim = self.news_dim + cfg.model.category_emb_dim + cfg.model.subcategory_emb_dim
         # 400 + 300 + 300
 
@@ -78,40 +78,31 @@ class PreNewsEncoder(nn.Module):
         """
 
 
-        #print("num_news的值是：{}".format(num_news))
-        #print("num_input的维度是：{}".format(news_input.shape))
 
-        # 分割news_input以获取各个部分的输入，假设category和subcategory位于最后两列
+
         title_input, _ ,category_input ,subcategory_input, _ = news_input.split([self.view_size[0], 5, 1, 1, 1], dim=-1)
 
-        #print("category_input的维度是：{}".format(category_input.shape))
-        #print("subcategory_input的维度是：{}".format(subcategory_input.shape))
+
 
         title_word_emb = self.word_encoder(title_input.long().view(-1, self.view_size[0]))
         category_emb = self.word_encoder(category_input.long().view(-1))
         subcategory_emb = self.word_encoder(subcategory_input.long().view(-1))
 
-        #print("title_word_emb的维度是：{}".format(title_word_emb.shape))
-        #print("category_emb的维度是：{}".format(category_emb.shape))
-        #print("subcategory_emb的维度是：{}".format(subcategory_emb.shape))
 
 
 
         total_word_emb = self.attetio(title_word_emb, mask)
 
-        #print("total_word_emb的维度是：{}".format(total_word_emb.shape)) #标题向量的维度不一样，先处理标题哈
+
 
         fuse_word_emb = torch.cat([total_word_emb, category_emb, subcategory_emb], dim=1)
 
-        #print("fuse_word_emb的维度是：{}".format(fuse_word_emb.shape))
-
+    
         last_word_emb = self.attention(fuse_word_emb, mask)
 
-        #print("last_word_emb的维度是：{}".format(last_word_emb.shape))
-
+ 
         result = self.last_encoder(last_word_emb)
 
-        #print("result的维度是：{}".format(result.shape))
-
+  
 
         return result
